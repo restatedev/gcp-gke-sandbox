8ab797d82260fa874c9f96632192ff5e39e82236
Luke Bond <luke.n.bond@gmail.com>
2026-05-18 10:13:56 +0100

    terraform fmt

--- storage.tf
+++ storage.tf
@@ -36,9 +36,9 @@ resource "kubernetes_storage_class_v1" "hyperdisk_balanced" {
 # deliver on the smallest VM NAP could land on.
 locals {
   hyperdisk_vacs = {
-    "hyperdisk-25k-400"   = { iops = "25000",  throughput = "400" }  # 4-vCPU profiles
-    "hyperdisk-50k-800"   = { iops = "50000",  throughput = "800" }  # 8-vCPU profiles
-    "hyperdisk-75k-1200"  = { iops = "75000",  throughput = "1200" } # 16-44 vCPU profiles (bounded by c3d; c3 could go higher in this range, but NAP may pick either)
+    "hyperdisk-25k-400"   = { iops = "25000", throughput = "400" }   # 4-vCPU profiles
+    "hyperdisk-50k-800"   = { iops = "50000", throughput = "800" }   # 8-vCPU profiles
+    "hyperdisk-75k-1200"  = { iops = "75000", throughput = "1200" }  # 16-44 vCPU profiles (bounded by c3d; c3 could go higher in this range, but NAP may pick either)
     "hyperdisk-160k-2400" = { iops = "160000", throughput = "2400" } # 60+ vCPU profiles (c3d catches up to c3 here)
   }
 }
